8f758d363159376764b5a26b308ad84c454f78c2
rakshikavats01 <89540965+rakshikavats01@users.noreply.github.com>
2024-05-22 09:36:37 +0000

    math and num

---
+++ 01_basics/09_numbers_and_maths.js
@@ -0,0 +1,29 @@
+const score1=300
+//console.log(score1)
+
+const score2=new Number(210)
+//console.log(score2)
+
+// console.log(score2.toString().length);
+// console.log(score2.toFixed(3));
+
+const score3=45.8463
+//console.log(score3.toPrecision(4));
+
+const score4=10000000
+//console.log(score4.toLocaleString('en-IN'))
+
+
+/****************Maths***************************/
+
+// console.log(Math)
+// console.log(Math.abs(-91))
+// console.log(Math.round(8.8))
+// console.log(Math.floor(-8.4))
+// console.log(Math.ceil(8.01))
+// console.log(Math.random())//btw 0-1
+// console.log(Math.random()*10)
+
+const min=10
+const max=20 
+console.log(Math.floor((Math.random()*(max-min+1))+min));
