704b970d668e2cfd35fffaea3c2d703151d56c7e
rakshikavats01 <89540965+rakshikavats01@users.noreply.github.com>
2024-05-29 10:22:32 +0000

    java1

---
+++ 03_basics/05_javascripBackground.js
@@ -0,0 +1,8 @@
+/*************Javascript Execution Context*************/
+//1.Global execution context:-this(value of this is window object in Javascript)
+//2.Function execution context
+//3.Eval execution context
+
+//Javascript code file runs in two phases:-
+//1.Memory creation phase/creation phase:-just allocate space to variables ,no execution
+//2.Execution phase
