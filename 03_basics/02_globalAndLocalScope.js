0277f57527741030e48551743aa64191003c5f02
rakshikavats01 <89540965+rakshikavats01@users.noreply.github.com>
2024-05-29 08:11:44 +0000

    scope2

--- 03_basics/02_globalAndLocalScope.js
+++ 03_basics/02_globalAndLocalScope.js
@@ -14,6 +14,37 @@ if(true){
 let a=10
 if(true){
     let a=50
-    console.log(a)
+    //console.log(a)
 }
-console.log(a)
+//console.log(a)
+
+//scope in a fucntion
+function user(){
+    const name="Rakshika"
+    function myAge(){
+        const age=10;
+        console.log(name)
+    }
+    //console.log(age)
+    //myAge()
+}
+user()//same scope mechanism in if-else too
+if(true){
+    const num1=10
+    if(true){
+        const num2=30
+        //console.log(num1+num2)
+    }
+    //console.log(num2-num1)
+}
+
+/**************INTERESTING*****************/
+function addOne(num1){
+    return num1+1
+}
+console.log(addOne(7))
+
+const addTwo=function(num){
+    return num+2
+}//this is known as the expression and also we can't access this before declaration
+console.log(addTwo(7))
