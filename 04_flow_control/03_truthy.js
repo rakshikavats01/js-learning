e4675ced6dff569a59a6cc80ae34a33f9745b595
rakshikavats01 <89540965+rakshikavats01@users.noreply.github.com>
2024-06-04 06:16:11 +0000

    truthy

---
+++ 04_flow_control/03_truthy.js
@@ -0,0 +1,30 @@
+const str1=""
+const str2="Raksh"
+// if(str1){
+//     console.log("str1 is not empty");
+// }else if(str2){
+//     console.log("str2 is not empty")
+// }else{
+//     console.log("both empty")
+// }
+
+//Falsy values:-
+//false,0,-0,BigInt 0n,"",NaN,null,undefined
+
+//truthy value
+//"0",'false'," ",{},[],fucntion(){}
+
+const emptyArray=[]
+const emptyObj={}
+// if(emptyArray.length===0){
+//     console.log("array is empty");
+// }
+// if(Object.keys(emptyObj).length===0){
+//     console.log("object is empty")
+// }
+
+
+//***********Interview purpose************/
+// console.log(false==0)
+// console.log(false=='')
+// console.log(''==0)
