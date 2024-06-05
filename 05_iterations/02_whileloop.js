36a6759136740b5f6ade3bb833d849ecd11af35f
rakshikavats01 <89540965+rakshikavats01@users.noreply.github.com>
2024-06-05 05:25:09 +0000

    while and do while

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,20 @@
+let index1=0
+while (index1<=10) {
+    //console.log(index1)
+    index1+=1
+}
+
+//using array
+let array=["raks","nanu","mamma","pappa"]
+let index2=0
+while(index2<array.length){
+    //console.log(array[index2])
+    index2+=1
+}
+
+/*************************Do while loop*******************/
+let score=1
+do{
+console.log(score)
+score++
+}while(score<=10)
